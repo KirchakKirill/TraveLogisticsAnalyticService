682c8314daf35fb5910bb6328c5527cd06735e89
kirchik <kirya.kirchak@mail.ru>
2024-10-17 14:27:22 +0300

    set properties

---
+++ src/main/java/com/example/TraveLogisticsAnalyticService/TraveLogisticsAnalyticServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.TraveLogisticsAnalyticService;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@SpringBootApplication
+public class TraveLogisticsAnalyticServiceApplication {
+
+	public static void main(String[] args) {
+	SpringApplication.run(TraveLogisticsAnalyticServiceApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/TraveLogisticsAnalyticService/TraveLogisticsAnalyticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TraveLogisticsAnalyticService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveLogisticsAnalyticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
